07bb0931d6b826716b60a504bf12203387f41d9f
usuario0888 <antonio.mejia@logiflowerp.com>
2025-05-07 21:29:08 -0500

    feat: initialize webhook server with TypeScript and Express
    - Added package.json to manage dependencies and scripts for the project.
    - Created src/index.ts as the main entry point for the webhook server, handling incoming POST requests and executing scripts.
    - Configured tsconfig.json for TypeScript compilation settings, including strict type checking and output directory.

---
+++ src/index.ts
@@ -0,0 +1,49 @@
+import express, { Request, Response } from 'express'
+import { exec } from 'child_process'
+import { get } from 'env-var'
+
+const app = express()
+const port = get('PORT').required().asPortNumber()
+const token = get('TOKEN').required().asString()
+const pathScripts = get('PATH_SCRIPTS').required().asString()
+
+app.post('/:script', (req: Request, res: Response) => {
+	console.log('Webhook recibido')
+
+	const { script } = req.params
+
+	if (!/^[a-zA-Z0-9_-]+$/.test(script)) {
+		res.status(400).send('Nombre de script inválido')
+		return
+	}
+
+	if (!req.query.token) {
+		res.status(403).send('No se envió el token')
+		return
+	}
+
+	if (req.query.token !== token) {
+		res.status(403).send('Token inválido')
+		return
+	}
+
+	const scriptPath = `${pathScripts}/${script}.sh`
+	console.log(`Ejecutando script: ${scriptPath}`)
+
+	exec(scriptPath, (error, stdout, stderr) => {
+		if (error) {
+			console.error(`Error ejecutando el script: ${error.message}`)
+			return res.status(500).send(`Error ejecutando el script: ${error.message}`)
+		}
+		if (stderr) {
+			console.error(`stderr: ${stderr}`)
+			return res.status(500).send(`stderr: ${stderr}`)
+		}
+		console.log(`stdout: ${stdout}`)
+		return res.status(200).send('Webhook procesado y script ejecutado con éxito')
+	})
+})
+
+app.listen(port, () => {
+	console.log(`Servidor escuchando en puerto ${port}`)
+})
